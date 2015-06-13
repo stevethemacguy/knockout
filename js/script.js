0ee18820fe40fc96a856371db9fc833952384a02
Steven Dunn <Dunn.Steven1@gmail.com>
2015-06-12 21:21:51 -0700

    More stuff.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
     // JS Class to represent a row in the seat reservations grid
+
+    this.totalPrice =0;
+
     function SeatReservation(name, initialMeal) {
         var self = this; //Using self instead of "this" is a common design pattern to avoid “this” conflicts.
         self.name = name;
         self.meal = ko.observable(initialMeal);
+        self.formattedPrice = ko.computed(function() {
+            var price = self.meal().price;
+            return price ? "$" + price.toFixed(2) : "None";
+        });
     }
 
     // Overall viewmodel for this screen, along with initial state
@@ -24,9 +31,20 @@ $(document).ready(function() {
             new SeatReservation("Bert", self.availableMeals[2])
         ]);
 
+        //Access this in the html like totalCharge(). Needs parenthesis because it's a computed value
+        self.totalCharge = ko.computed(function() {
+            var total = 0;
+            for (var i = 0; i < self.seats().length; i++)
+                total += self.seats()[i].meal().price;
+            return total;
+        });
 
         self.addSeat = function() {
             self.seats.push(new SeatReservation("", self.availableMeals[0]));
+        };
+
+        self.removeSeat= function(seat) {
+            self.seats.remove(seat);
         };
     }
 
